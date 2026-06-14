94065aebcfd3153547c059a72aa5be87f08053e6
KristianZupka <i23zupkkr@oakm.cz>
2026-06-14 04:06:26 +0200

    3. hodina - registrace login a session
    yo

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 require("dotenv").config();
 
+const session = require("express-session");
 const express = require("express");
+const path = require("path");
+
+
+const authRoutes = require("./routes/authRoutes");
 const connectDB = require("./config/db");
 
 const app = express();
 
 connectDB();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+
+    res.locals.user = req.session.user || null;
+
+    next();
+
+});
+
+app.use("/", authRoutes);
+
+app.get("/teams", (req, res) => {
+
+    res.send("Dashboard Team Managera");
+
+});
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -16,3 +49,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server běží na http://localhost:${PORT}`);
 });
+
+
+
+
